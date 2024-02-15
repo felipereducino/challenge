7026a27f7d04ddf1a52444871496d270f6185eb5
Felipe Reducino <felipe.reducino@fretebras.com.br>
2024-02-15 13:43:49 -0300

    feat: adds test for login component

--- src/Pages/Login/login.test.tsx
+++ src/Pages/Login/login.test.tsx
@@ -1,3 +1,32 @@
-test('renders example component', () => {
-    expect(true).toBe(true);
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Login from './login';
+
+describe('Login Component', () => {
+    it('renders Login component with all elements', () => {
+        const { queryAllByText, getByLabelText, getByRole } = render(
+            <Router>
+                <Login />
+            </Router>
+        );
+
+        // Check if at least one element with the text 'Login' is present.
+        const loginElements = queryAllByText('Login');
+        expect(loginElements.length).toBeGreaterThan(0);
+
+        // Check if the title is rendered.
+        //expect(getByText('Login')).toBeInTheDocument();
+
+        // Check if the email input is rendered.
+        expect(getByLabelText('E-mail')).toBeInTheDocument();
+
+        // Check if the password input is rendered.
+        expect(getByLabelText('Password')).toBeInTheDocument();
+
+        // Check if the login button is rendered.
+        const loginButton = getByRole('button', { name: 'Login' });
+        expect(loginButton).toBeInTheDocument();
+    });
 });
